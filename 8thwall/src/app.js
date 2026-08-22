929ba08c91e064677721725e25811c73ddf28515
THEEXPERIENC <alexaandresmartinezgonzalez@gmail.com>
2026-08-22 16:07:56 -0500

    AR test

--- 8thwall/src/app.js
+++ 8thwall/src/app.js
@@ -1,9 +1,8 @@
 const onxrloaded = () => {
   XR8.XrController.configure({
     imageTargetData: [
-      require('../image-targets/TARJETA.json'),
+      require('../image-targets/TARJETA.json')
     ],
   })
 }
-
 window.XR8 ? onxrloaded() : window.addEventListener('xrloaded', onxrloaded)
