0abe28aed5855ce1bcd8f857a9481ad284a40778
THEEXPERIENC <alexaandresmartinezgonzalez@gmail.com>
2026-08-24 02:32:27 -0500

    ARTT01

--- 8thwall/src/video-control.ts
+++ 8thwall/src/video-control.ts
@@ -1,22 +1,25 @@
 import * as ecs from '@8thwall/ecs'
 
-let isPlaying = true
+let isPlaying = false
 
 ecs.registerComponent({
   name: 'video-control',
   schema: {},
   add: (world, component) => {
+    
     const toggleVideo = () => {
-      isPlaying = !isPlaying
       const videos = document.querySelectorAll('video')
 
       videos.forEach((v) => {
-        if (!isPlaying) {
+        if (isPlaying) {
           v.pause()
         } else {
+          v.muted = false
           v.play().catch(() => {})
         }
       })
+
+      isPlaying = !isPlaying
     }
 
     world.events.addListener(component.eid, ecs.input.SCREEN_TOUCH_START, toggleVideo)
