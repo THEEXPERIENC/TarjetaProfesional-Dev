04abae23a60238d5f7f17e4b6df9a9e56d20841e
THEEXPERIENC <alexaandresmartinezgonzalez@gmail.com>
2026-08-24 10:54:25 -0500

    ARTXTX08

--- 8thwall/src/video-control.ts
+++ 8thwall/src/video-control.ts
@@ -1,28 +1,36 @@
 import * as ecs from '@8thwall/ecs'
 
+// Requerir la ruta real del archivo subido a assets
+const videoAsset = require('./assets/animacion-realidadV.mp4')
+
 ecs.registerComponent({
   name: 'video-control',
-  schema: {
-    videoUrl: ecs.string,
-  },
-  schemaDefaults: {
-    videoUrl: 'animacion-realidadV.mp4',
-  },
+  schema: {},
   add: (world, component) => {
     let isPlaying = true
 
-    const togglePlay = () => {
-      const videoElements = document.querySelectorAll('video')
-      isPlaying = !isPlaying
+    // Buscar el elemento de video existente creado por 8th Wall o asignarlo si no existe
+    const video = document.createElement('video')
+    video.src = videoAsset
+    video.crossOrigin = 'anonymous'
+    video.loop = true
+    video.playsInline = true
+    video.setAttribute('playsinline', '')
 
-      videoElements.forEach((video) => {
-        if (!isPlaying) {
-          video.pause()
-        } else {
-          video.muted = false
-          video.play().catch(() => {})
-        }
-      })
+    video.play().catch(() => {
+      video.muted = true
+      video.play()
+    })
+
+    const togglePlay = () => {
+      if (isPlaying) {
+        video.pause()
+        isPlaying = false
+      } else {
+        video.muted = false
+        video.play().catch(() => {})
+        isPlaying = true
+      }
     }
 
     world.events.addListener(component.eid, ecs.input.SCREEN_TOUCH_START, togglePlay)
